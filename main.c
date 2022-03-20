b8756afdf4cf1708fc11653cd610bca55702b9ab
Vladislav <vs.melnichuk09@gmail.com>
2022-03-20 14:15:06 +0300

    11

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#include "mail_lib/mail.h" // "mail" doesn't work
+#include "mail_lib/mail.h"  // "mail" doesn't work
 
 int main(void) {
     FILE* file = stdin;
-    Mail *new_mail = NULL;
+    Mail* new_mail = NULL;
 
     create_mail_pointer_to_struct(file, &new_mail);
     print_mail(new_mail);
