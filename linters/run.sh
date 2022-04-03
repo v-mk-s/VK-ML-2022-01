71db6d3dc2fae23d4654ceae7e3799bbb6420d24
Vladislav <vs.melnichuk09@gmail.com>
2022-04-03 12:26:38 +0300

    review_0.9

--- linters/run.sh
+++ linters/run.sh
@@ -1,5 +1,3 @@
-# author: v-mk-s | copyright 2022
-
 #!/usr/bin/env bash
 
 set -e
@@ -9,33 +7,33 @@ function print_header() {
     echo -e "\n***** ${1} *****"
 }
 
-function check_log() {
-    LOG=$( { ${1}; } 2>&1 )
-    STATUS=$?
-    echo "$LOG"
-    if echo "$LOG" | grep -q -E "${2}"
-    then
-        exit 1
-    fi
+# function check_log() {
+#     LOG=$( { ${1}; } 2>&1 )
+#     STATUS=$?
+#     echo "$LOG"
+#     if echo "$LOG" | grep -q -E "${2}"
+#     then
+#         exit 1
+#     fi
 
-    if [ $STATUS -ne 0 ]
-    then
-        exit $STATUS
-    fi
-}
+#     if [ $STATUS -ne 0 ]
+#     then
+#         exit $STATUS
+#     fi
+# }
 
 print_header "RUN clang-format"
-check_log "clang-format -style=google -i **/*.c **/*.h" "\(information\)"
+clang-tidy project/src/* project/include/* -warnings-as-errors=* -extra-arg=-std=c99 -- -Iproject/include
 print_header "SUCCESSFULLY DONE clang-format"
 print_header "SUCCESS 1/3"
 
 print_header "RUN cppcheck"
-check_log "cppcheck **/*.c IT--enable=all --inconclusive --error-exitcode=1 -Iprocessing_sequential -Iinterface -Iprocessing_parallel --suppress=missingIncludeSystem" "\(information\)"
+cppcheck project --enable=all --inconclusive --error-exitcode=1 -I project/include -I project/tests/include --suppress=missingIncludeSystem
 print_header "SUCCESSFULLY DONE cppcheck"
 print_header "SUCCESS 2/3"
 
 print_header "RUN cpplint"
-check_log "cpplint --extensions=c **/*.c" "ERROR! Cannot be opened for reading (r)."
+python3 ./linters/cpplint/cpplint.py --extensions=c project/include/* project/src/*
 print_header "SUCCESSFULLY DONE cpplint"
 print_header "SUCCESS 3/3"
 
