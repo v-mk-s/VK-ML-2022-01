8fe7d05af4da726e96dfb881509b7727d605fae2
Vladislav <vs.melnichuk09@gmail.com>
2022-04-03 13:47:50 +0300

    review_1.0

---
+++ build/processing_sequential/CMakeFiles/project_processing_sequential.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_processing_sequential.dir/processing_sequential.c.o"
+  "libproject_processing_sequential.a"
+  "libproject_processing_sequential.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project_processing_sequential.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tests/CMakeFiles/tests.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/e/_Vladik/6-Mail_Technopark/3-ML-developer/3-C&C++_advanced/2-HW/1/VK-ML-2022-01/tests/test.cpp" "/mnt/e/_Vladik/6-Mail_Technopark/3-ML-developer/3-C&C++_advanced/2-HW/1/VK-ML-2022-01/build/tests/CMakeFiles/tests.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../interface_main"
+  "../processing_sequential"
+  "../interface_main/PARENT_SCOPE"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/e/_Vladik/6-Mail_Technopark/3-ML-developer/3-C&C++_advanced/2-HW/1/VK-ML-2022-01/build/interface_main/CMakeFiles/project_interface_main.dir/DependInfo.cmake"
+  "/mnt/e/_Vladik/6-Mail_Technopark/3-ML-developer/3-C&C++_advanced/2-HW/1/VK-ML-2022-01/build/processing_sequential/CMakeFiles/project_processing_sequential.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
